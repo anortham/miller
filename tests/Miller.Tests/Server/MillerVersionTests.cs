db6308e72c56249bfe68f55f19d869da53b6da26
Alan Northam <anortham@gmail.com>
2026-08-24 11:19:06 -0500

    release: prepare Miller v1.21.2

--- tests/Miller.Tests/Server/MillerVersionTests.cs
+++ tests/Miller.Tests/Server/MillerVersionTests.cs
@@ -21,7 +21,7 @@ public sealed class MillerVersionTests
     [Fact]
     public void Current_StartsWithTheBaseVersion()
     {
-        Assert.StartsWith("1.21.1", MillerVersion.Current);
+        Assert.StartsWith("1.21.2", MillerVersion.Current);
     }
 
     [Fact]
